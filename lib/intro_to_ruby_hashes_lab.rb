c2124e1ee566c43cbaf289588524abdce1fb6ec4
Joseph Kofler <koflerj93@gmail.com>
2020-07-09 14:09:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -2,8 +2,7 @@ def my_hash_creator(key, value)
   object = {
     key : value
   }
-  return object
-  # return a hash that includes the key and value parameters passed into this method
+  object
 end
 
 def read_from_hash(hash, key)
